f06321f65b30bc648fb5b17cf8004c26c3b77bef
Aswathisunil23 <90394317+Aswathisunil23@users.noreply.github.com>
2022-02-14 17:13:04 +0530

    Create EmployeeContextModelSnapshort.cs

---
+++ Codes/Migrations/EmployeeContextModelSnapshort.cs
@@ -0,0 +1,39 @@
+using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+using assignmentcore.Model;
+
+namespace assignmentcore.Migrations
+{
+    [DbContext(typeof(EmployeeContext))]
+    partial class EmployeeContextModelSnapshot : ModelSnapshot
+    {
+        protected override void BuildModel(ModelBuilder modelBuilder)
+        {
+#pragma warning disable 612, 618
+            modelBuilder
+                .HasAnnotation("ProductVersion", "3.1.9")
+                .HasAnnotation("Relational:MaxIdentifierLength", 128)
+                .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
+
+            modelBuilder.Entity("assignmentcore.Model.Employee", b =>
+                {
+                    b.Property<Guid>("Id")
+                        .ValueGeneratedOnAdd()
+                        .HasColumnType("uniqueidentifier");
+
+                    b.Property<string>("Name")
+                        .IsRequired()
+                        .HasColumnType("nvarchar(10)")
+                        .HasMaxLength(10);
+
+                    b.HasKey("Id");
+
+                    b.ToTable("Employees");
+                });
+#pragma warning restore 612, 618
+        }
+    }
+}

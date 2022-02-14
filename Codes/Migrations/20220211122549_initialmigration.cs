74cd6b48c514655c65c6cd1b04be86477ebbbfd2
Aswathisunil23 <90394317+Aswathisunil23@users.noreply.github.com>
2022-02-14 17:15:50 +0530

    Create 20220211122549_initialmigration.cs

---
+++ Codes/Migrations/20220211122549_initialmigration.cs
@@ -0,0 +1,29 @@
+using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace assignmentcore.Migrations
+{
+    public partial class IntialMigrtio : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Employees",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Name = table.Column<string>(maxLength: 10, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employees", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Employees");
+        }
+    }
+}
